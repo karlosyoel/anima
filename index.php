c384b0fa135099193021f5dfa1066b1a31da3325
karlosyoel123 <82049853+karlosyoel123@users.noreply.github.com>
2022-04-24 21:14:53 -0500

    Initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  <meta charset="UTF-8">
+  <title>Universe</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="./otros/style.css">
+  
+
+</head>
+<body wfd-invisible="true">
+<!-- partial:index.partial.html -->
+<div id="canvas_container"><canvas style="width: 727px; height: 703px;" tabindex="0" width="908" height="878"></canvas></div>
+
+<!-- <button id="fullscr">Go Fullscreen</button> -->
+<!-- partial -->
+  <script src="./otros/three.min.js" wfd-invisible="true"></script>
+<script src="./otros/OrbitControls.js" wfd-invisible="true"></script>
+<script src="./otros/simplex-noise.min.js" wfd-invisible="true"></script><script src="./otros/script.js" wfd-invisible="true"></script>
+
+
+
+</body></html>
---
+++ new/index.js
@@ -0,0 +1,6 @@
+import * as THREE from '../node_modules/three/src/Three.js';
+
+export {THREE};
+// const scene = new Scene();
+
+
