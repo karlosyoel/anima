0038d1b512e713fc2c26a94b15e8fe913a6de4a1
karlosyoel123 <82049853+karlosyoel123@users.noreply.github.com>
2022-04-25 20:29:26 -0500

    Add imgs

--- otros/script.js
+++ otros/script.js
@@ -49,7 +49,7 @@ function init() {
     const loader = new THREE.TextureLoader();
     const textureSphereBg = loader.load('https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg');
     const texturenucleus1 = loader.load('https://i.ibb.co/hcN2qXk/star-nc8wkw.jpg');
-    const texturenucleus = loader.load('../imgs/earth.jpeg');
+    const texturenucleus = loader.load('./imgs/earth.jpeg');
     const textureStar = loader.load("https://i.ibb.co/ZKsdYSz/p1-g3zb2a.png");
     const texture1 = loader.load("https://i.ibb.co/F8by6wW/p2-b3gnym.png");  
     const texture2 = loader.load("https://i.ibb.co/yYS2yx5/p3-ttfn70.png");
